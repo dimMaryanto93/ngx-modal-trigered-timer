7e3eb38fa1b6ca7a6221f43cde0f063c0d34ebdc
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-12-13 13:50:31 +0700

    membuat modal autotrigered

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, TemplateRef, ViewChild} from '@angular/core';
+import {BsModalRef, BsModalService} from 'ngx-bootstrap';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  modalRef: BsModalRef;
+
+  @ViewChild('template')
+  modal: ElementRef;
+
+  constructor(private modalService: BsModalService) {
+
+
+    let timer = Observable.interval(1000);
+
+    timer.subscribe(
+      function (x) {
+        console.log('Next: ' + x);
+        if (x === 4)
+          modalService.show(this.modal);
+      },
+      function (err) {
+        console.log('Error: ' + err);
+      },
+      function () {
+        console.log('Completed');
+      });
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {ModalModule} from 'ngx-bootstrap';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
